7df0f56b0bfef2be400ac1c2e86a0f8c3b8c3527
Cooper Cook <36387492+coopertcook@users.noreply.github.com>
2026-06-06 20:31:37 -0700

    Make work with GIFT refactor

--- run_simulation.py
+++ run_simulation.py
@@ -2,11 +2,11 @@ import time
 from copy import deepcopy
 from rich import print
 
-from gift.GIFT.core.aircraft import Aircraft
-from gift.GIFT.core.simulator import Simulator
-from gift.GIFT.processing.animator import Animation
-from gift.GIFT.processing.plotter import SimulationPlotter
-from gift.GIFT.utils.utilities import TrajectoryData, standardize_simulator_data
+from GIFT.core.aircraft import Aircraft
+from GIFT.core.simulator import Simulator
+from GIFT.processing.animator import Animation
+from GIFT.processing.plotter import SimulationPlotter
+from GIFT.utils.utilities import TrajectoryData, standardize_simulator_data
 
 
 animator_type: str = "matplotlib"
@@ -16,10 +16,10 @@ vehicle: str = "raptor_glider_3dof"
 time_start = time.time()
 
 aircraft = Aircraft(type=vehicle, config_overrides={})
-simulation = Simulator(aircraft, dynamics="LPV3")
+simulation = Simulator(aircraft, dynamics="3DOF")
 LPV_dynamics = Simulator(aircraft, enable_logging=False, dynamics="LPV3").dynamics
 
-raw_sim_data = simulation.execute()
+raw_sim_data = simulation.execute(t_start=1, t_end=2)
 sim_data = standardize_simulator_data(raw_sim_data, aircraft=aircraft)
 
 result = TrajectoryData(
@@ -31,6 +31,7 @@ time_end = time.time()
 print(f">>> Simulation completed in {time_end - time_start:.2f} seconds")
 
 if visualize:
+    result.data = result.data.drop_nulls()
     # use the results dictionary to visualize the data
     plots = SimulationPlotter(
         results=[result],
@@ -40,4 +41,3 @@ if visualize:
 
     # print(">>> Starting Matplotlib animation...")
     # Animation([result]).animate(speed=0.4)
-
